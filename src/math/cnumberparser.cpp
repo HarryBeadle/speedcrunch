a5e53066083e84fd154cc29c2bf4a061b5562d07
Hadrien Theveneau <theveneau@gmail.com>
2016-01-25 09:59:14 +0100

    Change headers of some files to match typical Speedcrunch files headers.

--- src/math/cnumberparser.cpp
+++ src/math/cnumberparser.cpp
@@ -1,3 +1,57 @@
+// cnumberparser.cpp
+// Complex number support : complex number parser using a recursive
+// descent approach.
+//
+// This file is part of the SpeedCrunch project
+// Copyright (C) 2013, 2015-2016 Hadrien Theveneau <theveneau@gmail.com>.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; see the file COPYING.  If not, write to
+// the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+
+// This complex number parser currently recognizes the following forms :
+//
+//   a
+//   a+ib
+//   a+jb
+//   a+bi
+//   a+bj
+//   ib
+//   jb
+//   bi
+//   bj
+//
+// The grammar describing one complex number is:
+//
+//   complex_number -> part suite
+//   suite -> []
+//   suite -> + part suite
+//
+//   part -> prefixed_part
+//   part -> postfixed_part
+//
+//   part_prefixed -> i base_number
+//   part_prefixed -> j base_number
+//
+//   part_postfixed -> base_number postfix
+//
+//   postfix -> i
+//   postfix -> j
+//   postfix -> []
+
+
 #include "cnumberparser.h"
 
 using namespace CNumberParserExceptions;
