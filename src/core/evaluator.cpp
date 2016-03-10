cf4b1bfa903ab43571d704ff3d3c1d3199ca6eb9
Teyut <teyut@free.fr>
2016-03-10 19:26:21 +0100

    Handle token stack underflow error when compiling. Removed useless constant condition in Evaluator::compile().

--- src/core/evaluator.cpp
+++ src/core/evaluator.cpp
@@ -166,10 +166,13 @@ public:
     void push(const Token& token);
     const Token& top();
     const Token& top(unsigned index);
+    bool hasError() const { return !m_error.isEmpty(); }
+    QString error() const { return m_error; }
 
 private:
     void ensureSpace();
     int topIndex;
+    QString m_error;
 };
 
 const Token Token::null;
@@ -306,6 +309,7 @@ QString Token::description() const
 TokenStack::TokenStack() : QVector<Token>()
 {
     topIndex = 0;
+    m_error = "";
     ensureSpace();
 }
 
@@ -327,7 +331,11 @@ void TokenStack::push(const Token& token)
 
 Token TokenStack::pop()
 {
-    return topIndex > 0 ? Token(at(--topIndex)) : Token();
+    if (topIndex > 0)
+        return Token(at(--topIndex));
+
+    m_error = Evaluator::tr("token stack is empty (BUG)");
+    return Token();
 }
 
 const Token& TokenStack::top()
@@ -852,7 +860,7 @@ void Evaluator::compile(const Tokens& tokens)
     QStack<int> argStack;
     unsigned argCount = 1;
 
-    for (int i = 0; i <= tokens.count(); ++i) {
+    for (int i = 0; i <= tokens.count() && !syntaxStack.hasError(); ++i) {
         // Helper token: InvalidOp is end-of-expression.
         Token token = (i < tokens.count()) ? tokens.at(i) : Token(Token::stxOperator);
         Token::Type tokenType = token.type();
@@ -869,13 +877,12 @@ void Evaluator::compile(const Tokens& tokens)
             break;
 
         // Try to apply all parsing rules.
-        if (1) { // TODO: Remove this
 #ifdef EVALUATOR_DEBUG
             dbg << "\tChecking rules..." << "\n";
 #endif
             // Repeat until no more rule applies.
             bool argHandled = false;
-            while (true) {
+            while (!syntaxStack.hasError()) {
                 bool ruleFound = false;
 
                 // Rule for function last argument: id (arg) -> arg.
@@ -1197,12 +1204,13 @@ void Evaluator::compile(const Tokens& tokens)
                 dbg << "\tPush " << token.asNumber() << " to constant pools" << "\n";
 #endif
             }
-        }
     }
 
-    // syntaxStack must left only one operand and end-of-expression (i.e. InvalidOp).
     m_valid = false;
-    if (syntaxStack.itemCount() == 2 && syntaxStack.top().isOperator()
+    if (syntaxStack.hasError())
+        m_error = syntaxStack.error();
+    // syntaxStack must left only one operand and end-of-expression (i.e. InvalidOp).
+    else if (syntaxStack.itemCount() == 2 && syntaxStack.top().isOperator()
          && syntaxStack.top().asOperator() == Token::InvalidOp
          && !syntaxStack.top(1).isOperator())
     {
@@ -1285,6 +1293,11 @@ CNumber Evaluator::evalNoAssign()
         }
 
         compile(tokens);
+        if (!m_valid) {
+            if (m_error.isEmpty())
+                m_error = tr("compile error");
+            return CNumber(0);
+        }
     }
 
     result = exec(m_codes, m_constants, m_identifiers);
