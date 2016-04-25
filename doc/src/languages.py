e4d7ae55acffd1d700ee2d553634a07666756e50
Felix Krull <f_krull@gmx.de>
2016-04-25 20:50:55 +0200

    Re-encode languages.py to UTF-8.

--- doc/src/languages.py
+++ doc/src/languages.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
-# -*- coding: latin-1 -*-
+# -*- coding: utf-8 -*-
 
 from __future__ import print_function, unicode_literals
 
 SOURCE_LANGUAGE = 'en_US'
 LANGUAGES = {
-            'en_US': 'English',
-            'de_DE': 'Deutsch',
-            'es_ES': 'Español',
-            'fr_FR': 'Français'
-            }                 
+    'en_US': 'English',
+    'de_DE': 'Deutsch',
+    'es_ES': 'EspaÃ±ol',
+    'fr_FR': 'FranÃ§ais'
+}
 
 LANGUAGE_CODES = LANGUAGES.keys()
 
