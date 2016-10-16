HarryBeadle/speedcrunch

commit c16441e9543c2c372eec9abe0f6789436ea9ab9b

Reset bitfield on expression and history clearing (issue #654)

src/gui/mainwindow.cpp
@@ -814,7 +814,7 @@ void MainWindow::createFixedConnections()
     connect(m_actions.sessionQuit, SIGNAL(triggered()), SLOT(close()));
     connect(m_actions.sessionSave, SIGNAL(triggered()), SLOT(saveSessionDialog()));
 
-    connect(m_actions.editClearExpression, SIGNAL(triggered()), SLOT(clearEditor()));
+    connect(m_actions.editClearExpression, SIGNAL(triggered()), SLOT(clearEditorAndBitfield()));
     connect(m_actions.editClearHistory, SIGNAL(triggered()), SLOT(clearHistory()));
     connect(m_actions.editCopyLastResult, SIGNAL(triggered()), SLOT(copyResultToClipboard()));
     connect(m_actions.editCopy, SIGNAL(triggered()), SLOT(copy()));
@@ -1213,7 +1213,7 @@ void MainWindow::showAboutDialog()
 void MainWindow::clearHistory()
 {
     m_session->clearHistory();
-    clearEditor();
+    clearEditorAndBitfield();
     emit historyChanged();
 }
 
@@ -1221,6 +1221,12 @@ void MainWindow::clearEditor()
 {
     m_widgets.editor->clear();
     m_widgets.editor->setFocus();
+}
+
+void MainWindow::clearEditorAndBitfield()
+{
+    clearEditor();
+    m_widgets.bitField->clear();
 }
 
 void MainWindow::copyResultToClipboard()

src/gui/mainwindow.h
@@ -80,6 +80,7 @@ public slots:
 private slots:
     void applySelectedColorScheme();
     void clearEditor();
+    void clearEditorAndBitfield();
     void clearHistory();
     void copyResultToClipboard();
     void cycleAngleUnits();

src/gui/bitfieldwidget.cpp
@@ -191,11 +191,15 @@ void BitFieldWidget::invertBits()
     onBitChanged();
 }
 
+void BitFieldWidget::clear()
+{
+    for (auto w : m_bitWidgets)
+        w->setState(false);
+}
+
 void BitFieldWidget::resetBits()
 {
-    foreach (BitWidget* w, m_bitWidgets)
-        w->setState(false);
-
+    clear();
     onBitChanged();
 }
 

src/gui/bitfieldwidget.h
@@ -62,9 +62,10 @@ signals:
     void bitsChanged(const QString&);
 
 protected:
-    virtual void wheelEvent(QWheelEvent *we);
+    virtual void wheelEvent(QWheelEvent*);
 
 public slots:
+    void clear();
     void updateBits(const Quantity&);
 
 private slots:

commit f797f3f4be03cdd112e97847b9f320de9eda51cf

Fix crash introduced by commit c16441e
We need to check whether dynamic components have been created before
using them.

src/gui/mainwindow.cpp
@@ -1226,7 +1226,8 @@ void MainWindow::clearEditor()
 void MainWindow::clearEditorAndBitfield()
 {
     clearEditor();
-    m_widgets.bitField->clear();
+    if (m_widgets.bitField)
+        m_widgets.bitField->clear();
 }
 
 void MainWindow::copyResultToClipboard()

src/gui/mainwindow.h
@@ -341,10 +341,10 @@ private:
         QLabel* state;
         ResultDisplay* display;
         Editor* editor;
-        Keypad* keypad;
+        Keypad* keypad = nullptr;
         QWidget* root;
-        ManualWindow* manual;
-        BitFieldWidget* bitField;
+        ManualWindow* manual = nullptr;
+        BitFieldWidget* bitField = nullptr;
     } m_widgets;
 
     struct {